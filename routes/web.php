f8e055fcbc9e6186aad98815362798e9fb2f81ae
boomboomlei <1846352100@qq.com>
2023-11-28 14:06:37 +0000

    用户注册

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get("/","StaticPagesController@home")->name('home');
 Route::get("/help","StaticPagesController@help")->name('help');
 Route::get("/about","StaticPagesController@about")->name('about');
 Route::get("test","StaticPagesController@test")->name('test');
+
+Route::get('signup','UsersController@create')->name('signup');
