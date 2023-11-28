ded00036b559097d72cf789714edf2b69b714fc3
boomboomlei <1846352100@qq.com>
2023-11-28 13:45:09 +0000

    路由命名

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get("/","StaticPagesController@home");
-Route::get("/help","StaticPagesController@help");
-Route::get("/about","StaticPagesController@about");
-Route::get("test","StaticPagesController@test");
+Route::get("/","StaticPagesController@home")->name('home');
+Route::get("/help","StaticPagesController@help")->name('help');
+Route::get("/about","StaticPagesController@about")->name('about');
+Route::get("test","StaticPagesController@test")->name('test');
