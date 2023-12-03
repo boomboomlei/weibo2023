014a21297319558fa448f7dd5215649b588cdbff
boomboomlei <1846352100@qq.com>
2023-12-03 01:09:10 +0000

    add crsf 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,4 +15,12 @@ class UsersController extends Controller
        // echo compact('user');
         return view('users.show',compact('user'));
     }
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'=>'required|unique:users|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
