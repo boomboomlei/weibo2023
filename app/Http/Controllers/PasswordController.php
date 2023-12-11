935e682ded40f4000c65936e896dc0ed1f505b9d
boomboomlei <1846352100@qq.com>
2023-12-11 00:27:37 +0000

    访问限流

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -10,6 +10,12 @@ use Mail;
 use Carbon\Carbon;
 
 class PasswordController extends Controller{
+
+    public function __construct(){
+        $this->middleware('throttle:2,1',[
+            'only'=>['showLinkRequestForm']
+        ]);
+    }
     public function showLinkRequestForm(){
         return view('auth.passwords.email');
     }
@@ -36,6 +42,50 @@ class PasswordController extends Controller{
         });
 
         session()->flash('success','重置邮箱发送成功，请查收');
+        return redirect()->back();
+    }
+
+    public function showResetForm(Request $request){
+        // $token=$request->route()->parameter('token');
+        $token=$request->token;
+        return view('auth.passwords.reset',compact('token'));
+    }
+
+    public function reset(Request $request){
+        $request->validate([
+            'token'=>'required',
+            'email'=>'required|email',
+            'password'=>'required|confirmed|min:8',
+        ]);
+        $email=$request->email;
+        $token=$request->token;
+        $expires=60*10;
+
+        $user=User::where('email',$email)->first();
+        if(is_null($user)){
+            session()->flash("danger","邮箱未注册");
+            return redirect()->back()->withInput();
+        }
+
+        $record=(array)DB::table('password_restes')->where('email',$email)->first();
+        if($record){
+            if(Carbon::parse($record['created_at'])->addSeconds($expires)->isPast()){
+                session()->flash('danger','连接过期，请重新尝试。。');
+                return redirect()->back();
+            }
+            if(!Hash::check($token,$record['token'])){
+                session()->flash('danger','令牌错误');
+                return redirect()->back();
+            }
+
+            $user->update([
+                'password'=>bcrypt($request->password)
+            ]);
+
+            session()->flash('success','密码重置成功');
+            return redirect()->route('login');
+        }
+        session()->flash('danger','未找到重置记录');
         return redirect()->back();
     }
 
